fc12b0173407058a71986ca6472bcd493a14f95e
leventsamlioglu <leventsamlioglu@icloud.com>
2022-05-23 18:45:16 +0200

    execution

--- 01-execution context and lexical environment/app.js
+++ 01-execution context and lexical environment/app.js
@@ -1,10 +1,11 @@
-var a;
+function b() {
+    console.log('Called b!');
+}
+
+b();
+
 console.log(a);
 
+var a = 'Hello World!';
 
-if (a === undefined) {
-    console.log('a is undefined!');   
-}
-else {
-    console.log('a is defined!');   
-}
+console.log(a);
