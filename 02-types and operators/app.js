0b0cf1cbb207a7d22af6f15380facc6758e8aa7c
leventsamlioglu <leventsamlioglu@icloud.com>
2022-05-24 20:19:32 +0200

    comparison operators

--- 02-types and operators/app.js
+++ 02-types and operators/app.js
@@ -1,7 +1,8 @@
-var a;
+console.log(1 < 2 < 3); // true because 1 < 2 is true and true < 3 is true. (Number(true) => 1 >>> Coercion)
+console.log(3 < 2 < 1); // true because 3 < 2 is false and false < 1 is true. (Number(false) => 0 >>> Coercion)
 
-// goes to internet and looks for a value
+console.log(Number(undefined)); // NaN
+console.log(Number(null)); // 0
 
-if (a) {
-  console.log('Something is there.');  
-}
+console.log(null == 0); // false
+console.log(null < 1); // true
