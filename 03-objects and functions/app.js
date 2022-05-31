fa4894f0159d1667cec2aec16bc596289b244fee
leventsamlioglu <leventsamlioglu@icloud.com>
2022-05-31 17:44:49 +0200

    this

--- 03-objects and functions/app.js
+++ 03-objects and functions/app.js
@@ -1,33 +1,39 @@
-// by value (primitives)
-var a = 3;
-var b;
-
-b = a;
-a = 2;
-
-console.log(a);
-console.log(b);
-
-// by reference (all objects (including functions))
-var c = { greeting: 'hi' };
-var d;
-
-d = c;
-c.greeting = 'hello'; // mutate
-
-console.log(c);
-console.log(d);
-
-// by reference (even as parameters)
-function changeGreeting(obj) {
-    obj.greeting = 'Hola'; // mutate   
+function a() {
+    console.log(this);
+    this.newvariable = 'hello';
 }
 
-changeGreeting(d);
-console.log(c);
-console.log(d);
+var b = function() {
+    console.log(this);   
+}
 
-// equals operator sets up new memory space (new address)
-c = { greeting: 'howdy' };
-console.log(c);
-console.log(d);
+a();
+
+console.log(newvariable); // not good!
+
+b();
+
+var c = {
+    name: 'The c object',
+    log: function() {
+        var self = this;
+        
+        self.name = 'Updated c object';
+        console.log(self);
+        
+        var setname = function(newname) {
+            self.name = newname;   
+        }
+        setname('Updated again! The c object');
+        console.log(self);
+    }
+}
+
+c.log();
+
+
+
+
+
+
+
