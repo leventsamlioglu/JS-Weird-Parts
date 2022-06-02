03789597bc9f294d909147117aa480bbe346be68
leventsamlioglu <leventsamlioglu@icloud.com>
2022-06-02 09:52:35 +0200

    arguments

--- 03-objects and functions/app.js
+++ 03-objects and functions/app.js
@@ -1,17 +1,26 @@
-var arr = [
-    1, 
-    false, 
-    {
-        name: 'Tony',
-        address: '111 Main St.'
-    },
-    function(name) {
-        var greeting = 'Hello ';
-        console.log(greeting + name);
-    },
-    "hello"
-];
+function greet(firstname, lastname, language) {
+ 
+    language = language || 'en';
+    
+    if (arguments.length === 0) {
+        console.log('Missing parameters!');
+        console.log('-------------');
+        return;
+    }
+    
+    console.log(firstname);
+    console.log(lastname);
+    console.log(language);
+    console.log(arguments);
+    console.log('arg 0: ' + arguments[0]);
+    console.log('-------------');
+    
+}
 
-console.log(arr);
-arr[3](arr[2].name);
+greet();
+greet('John');
+greet('John', 'Doe');
+greet('John', 'Doe', 'es');
 
+// in ES6 I can do:  function greet(firstname, ...other)
+// and 'other' will be an array that contains the rest of the arguments
