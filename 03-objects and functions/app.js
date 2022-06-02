337aa72186c38984f263e220a559e5e241ed36f4
leventsamlioglu <leventsamlioglu@icloud.com>
2022-06-02 09:51:42 +0200

    arrays

--- 03-objects and functions/app.js
+++ 03-objects and functions/app.js
@@ -1,39 +1,17 @@
-function a() {
-    console.log(this);
-    this.newvariable = 'hello';
-}
+var arr = [
+    1, 
+    false, 
+    {
+        name: 'Tony',
+        address: '111 Main St.'
+    },
+    function(name) {
+        var greeting = 'Hello ';
+        console.log(greeting + name);
+    },
+    "hello"
+];
 
-var b = function() {
-    console.log(this);   
-}
-
-a();
-
-console.log(newvariable); // not good!
-
-b();
-
-var c = {
-    name: 'The c object',
-    log: function() {
-        var self = this;
-        
-        self.name = 'Updated c object';
-        console.log(self);
-        
-        var setname = function(newname) {
-            self.name = newname;   
-        }
-        setname('Updated again! The c object');
-        console.log(self);
-    }
-}
-
-c.log();
-
-
-
-
-
-
+console.log(arr);
+arr[3](arr[2].name);
 
