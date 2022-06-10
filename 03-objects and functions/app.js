71035831ac71ad11ad0cc16dd56ac9ee731c3506
leventsamlioglu <leventsamlioglu@icloud.com>
2022-06-10 22:19:34 +0200

    callbacks

--- 03-objects and functions/app.js
+++ 03-objects and functions/app.js
@@ -1,21 +1,44 @@
-function makeGreeting(language) {
+function sayHiLater() {
  
-    return function(firstname, lastname) {
-     
-        if (language === 'en') {
-            console.log('Hello ' + firstname + ' ' + lastname);   
-        }
-
-        if (language === 'es') {
-            console.log('Hola ' + firstname + ' ' + lastname);   
-        }
+    var greeting = 'Hi!';
+    
+    setTimeout(function() {
         
-    }
+        console.log(greeting);
+        
+    }, 3000);
     
 }
 
-var greetEnglish = makeGreeting('en');
-var greetSpanish = makeGreeting('es');
+sayHiLater();
 
-greetEnglish('John', 'Doe');
-greetSpanish('John', 'Doe');
+// jQuery uses function expressions and first-class functions!
+//$("button").click(function() {
+//    
+//});
+
+function tellMeWhenDone(callback) {
+ 
+    var a = 1000; // some work
+    var b = 2000; // some work
+    
+    callback(); // the 'callback', it runs the function I give it!
+    
+}
+
+tellMeWhenDone(function() {
+   
+    console.log('I am done!');
+    
+});
+
+tellMeWhenDone(function() {
+   
+    console.log('All done...');
+    
+});
+
+
+
+
+
