f606214555b923b800a326897aeff6d32b6873fd
leventsamlioglu <leventsamlioglu@icloud.com>
2022-06-14 19:13:15 +0200

    prototype

--- 03-objects and functions/app.js
+++ 03-objects and functions/app.js
@@ -1,82 +1,31 @@
-function mapForEach(arr, fn) {
-    
-    var newArr = [];
-    for (var i=0; i < arr.length; i++) {
-        newArr.push(
-            fn(arr[i])   
-        )
-    };
-    
-    return newArr;
+var person = {
+    firstname: 'Default',
+    lastname: 'Default',
+    getFullName: function() {
+        return this.firstname + ' ' + this.lastname;  
+    }
 }
 
-var arr1 = [1,2,3];
-console.log(arr1);
-
-
-var arr2 = mapForEach(arr1, function(item) {
-   return item * 2; 
-});
-console.log(arr2);
-
-
-var arr3 = mapForEach(arr1, function(item) {
-   return item > 2; 
-});
-console.log(arr3);
-
-
-var checkPastLimit = function(limiter, item) {
-    return item > limiter;   
+var john = {
+    firstname: 'John',
+    lastname: 'Doe'
 }
-var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1));
-console.log(arr4);
 
+// don't do this EVER! for demo purposes only!!!
+john.__proto__ = person;
+console.log(john.getFullName());
+console.log(john.firstname);
 
-var checkPastLimitSimplified = function(limiter) {
-    return function(limiter, item) {
-        return item > limiter;   
-    }.bind(this, limiter); 
-};
+var jane = {
+    firstname: 'Jane'   
+}
 
-var arr5 = mapForEach(arr1, checkPastLimitSimplified(1));
-console.log(arr5);
+jane.__proto__ = person;
+console.log(jane.getFullName());
 
+person.getFormalFullName = function() {
+    return this.lastname + ', ' + this.firstname;   
+}
 
-// underscore
-var arr6 = _.map(arr1, function(item) { return item * 3 });
-console.log(arr6);
-
-var arr7 = _.filter([2,3,4,5,6,7], function(item) { return item % 2 === 0; });
-console.log(arr7);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(john.getFormalFullName());
+console.log(jane.getFormalFullName());
