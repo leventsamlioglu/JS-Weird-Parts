6cc046a4582c986a16844558e6113a05fd3556fd
leventsamlioglu <leventsamlioglu@icloud.com>
2022-06-20 13:48:03 +0200

    object create

--- 04-building objects/app.js
+++ 04-building objects/app.js
@@ -1,9 +1,25 @@
-String.prototype.isLengthGreaterThan = function(limit) {
-    return this.length > limit;  
+// polyfill
+if (!Object.create) {
+  Object.create = function (o) {
+    if (arguments.length > 1) {
+      throw new Error('Object.create implementation'
+      + ' only accepts the first parameter.');
+    }
+    function F() {}
+    F.prototype = o;
+    return new F();
+  };
 }
 
-console.log("John".isLengthGreaterThan(3));
-
-Number.prototype.isPositive = function() {
-    return this > 0;   
+var person = {
+    firstname: 'Default',
+    lastname: 'Default',
+    greet: function() {
+        return 'Hi ' + this.firstname;   
+    }
 }
+
+var john = Object.create(person);
+john.firstname = 'John';
+john.lastname = 'Doe';
+console.log(john);
